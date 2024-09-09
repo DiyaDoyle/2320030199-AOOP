795eb6482225ce858cb6f59512635c5053f45df8
DIYA MAREEZA DOYLE <diyadoyle@gmail.com>
2024-09-09 19:12:14 +0530

    WEEK 3 1B

---
+++ Practical/ImageViewing/src/com/ImageViewing/Client.java
@@ -0,0 +1,13 @@
+package com.ImageViewing;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GalleryApp galleryApp= new GalleryApp();
+		galleryApp.Show("jpg");
+		galleryApp.Show("png");
+
+	}
+
+}
---
+++ Practical/ImageViewing/src/com/ImageViewing/PngShower.java
@@ -0,0 +1,18 @@
+package com.ImageViewing;
+
+public class PngShower implements AdvancedImageViewer{
+
+	@Override
+	public void showPng() {
+		// TODO Auto-generated method stub
+		System.out.println("Displaying Png Picture");
+
+	}
+
+	@Override
+	public void showJpg() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
