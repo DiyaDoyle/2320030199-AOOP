11db59b879aa3d227f78c3a1a4881f20865a7128
DIYA MAREEZA DOYLE <diyadoyle@gmail.com>
2024-09-10 08:25:40 +0530

    WEEK 4 1A

---
+++ CO1/Practical/LoggingSystemBehavioural/LogginSystemBehavioural/src/com/LogginSystem/DebugHandler.java
@@ -0,0 +1,17 @@
+package com.LogginSystem;
+
+public class DebugHandler extends LogHandler{
+
+	@Override
+	protected boolean canHandle(LogLevel level) {
+		// TODO Auto-generated method stub
+		return level== LogLevel.DEBUG;
+	}
+
+	@Override
+	protected void logMessage(String message) {
+		// TODO Auto-generated method stub
+		 System.out.println("DEBUG: " + message);
+	}
+
+}
