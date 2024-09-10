8b80e5a3fd4eadb479c20fbefeda97aaa47603ee
DIYA MAREEZA DOYLE <diyadoyle@gmail.com>
2024-09-10 08:10:29 +0530

    Delete StudentInformationSystem directory

--- StudentInformationSystem/src/com/StudentInformationsystem/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.StudentInformationsystem;
-
-public interface CourseRepository {
-	void addCourse(Course course);
-    Course getCourseById(String courseId);
-    void removeCourse(String courseId);
-
-}
--- StudentInformationSystem/src/com/StudentInformationsystem/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.StudentInformationsystem;
-
-public interface StudentRepository {
-	void addStudent(Student student);
-    Student getStudentById(String id);
-    void removeStudent(String id);
-
-}
