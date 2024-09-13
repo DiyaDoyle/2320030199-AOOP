bdfafeb22789f0f0d48c15c604038684bd0a0f08
DIYA MAREEZA DOYLE <diyadoyle@gmail.com>
2024-09-13 13:35:13 +0530

    Delete CO1/Project/FurnitureAbstractFactory/FunitureAbstractFactory directory

--- CO1/Project/FurnitureAbstractFactory/FunitureAbstractFactory/src/com/FurnitureFactory/Bed.java
+++
@@ -1,5 +0,0 @@
-package com.FurnitureFactory;
-
-public interface Bed {
-	public void lieOn();
-}
--- CO1/Project/FurnitureAbstractFactory/FunitureAbstractFactory/src/com/FurnitureFactory/Chair.java
+++
@@ -1,7 +0,0 @@
-package com.FurnitureFactory;
-
-public interface Chair {
-	
-	public void sitOn();
-
-}
