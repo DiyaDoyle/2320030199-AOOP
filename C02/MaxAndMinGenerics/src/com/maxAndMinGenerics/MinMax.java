8fa122c0e86cac22114d9d532c42c5b821296896
DIYA MAREEZA DOYLE <diyadoyle@gmail.com>
2024-09-13 13:43:49 +0530

    Delete C02/MaxAndMinGenerics directory

--- C02/MaxAndMinGenerics/src/com/maxAndMinGenerics/MinMax.java
+++
@@ -1,7 +0,0 @@
-package com.maxAndMinGenerics;
-
- public interface MinMax<T extends Comparable<T>> {
-	 T findMin(T[] array);
-	 T findMax(T[] array);
-
-}
--- C02/MaxAndMinGenerics/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module ManAndMinGenerics {
-}
